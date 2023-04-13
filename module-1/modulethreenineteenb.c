405845d4e1b9f107a200e971d839493b5477fee6
Hemilpatel24 <109792586+Hemilpatel24@users.noreply.github.com>
2023-04-13 17:23:16 +0530

    Add files via upload

---
+++ module-1/modulethreenineteenb.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    i = 51;
+
+    while(i <= 61)
+    {
+        printf("\n%d",i);
+        i++;
+    }
+    return 0;
+}
---
+++ module-1/modulethreenineteenc.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    i = 100;
+
+    do
+    {
+        printf("\n%d",i);
+        i--;
+    } while (i >= 81);
+
+    return 0;
+    
+}
