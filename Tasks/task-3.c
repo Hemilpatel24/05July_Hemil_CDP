07fee003191469f14243a35f7d1d6d4f18331f6d
Hemilpatel24 <109792586+Hemilpatel24@users.noreply.github.com>
2023-04-18 17:21:16 +0530

    Update task-3.c

--- Tasks/task-3.c
+++ Tasks/task-3.c
@@ -6,16 +6,13 @@ int main()
  printf("\n Enter Last Number : ");
  scanf("%d",&last);
 
- //  For loop
- // Code For Even Number List
   printf("\n Even Number List :\n ");
 
  for(n=2;n<=last;n+=2)
   {
     printf(" %d",n);
   }
-
- // Code For Odd Number List
+ 
  printf("\n\n Odd Number List :\n ");
 
  for(n=1;n<=last;n+=2)
