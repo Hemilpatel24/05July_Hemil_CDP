8aa0ea7c77f3032a8193b8ce7ac8e22abbc81547
Hemilpatel24 <109792586+Hemilpatel24@users.noreply.github.com>
2023-06-02 18:32:40 +0530

    Update assisment01.c

--- Assisment-01/assisment01.c
+++ Assisment-01/assisment01.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
+int no = 0;
+int bug()
+{
 
+	printf("How many burgur do you want: ");
+	scanf("%d", &no);
+	printf("Burger added to the bill.\n");
+         no *= 100;      	
+	return  0;
+}
+int piz()
+{
+
+	printf("How many pizza do you want: ");
+	scanf("%d", &no);
+	printf("Burger added to the bill.\n");
+         no *= 180;      	
+	return  0;
+}
+int dos()
+{
+
+	printf("How many dosa do you want: ");
+	scanf("%d", &no);
+	printf("Burger added to the bill.\n");
+         no *= 120;      	
+	return  0;
+	
+}
+int idl()
+{
+
+	printf("How many idli do you want: ");
+	scanf("%d", &no);
+	printf("Burger added to the bill.\n");
+         no *= 50;      	
+	return  0;
+}
 int main() {
     int choice;
     float bill = 0;
-    char c;
-    
+    char c[5];
+//Enter the food menu to choose from:    
     printf("Welcome to the Food Billing System!\n");
     printf("Menu:\n");
     printf("1. Burger - 100\n");
@@ -12,45 +49,50 @@ int main() {
     printf("3. Dosa - 120\n");
     printf("4. Idli - 50\n");
     printf("5. Exit\n");
-
-    FBS:
-    do {
+    
+// Enter your choice to continue with.
+        system:
+        	
         printf("Enter your choice (1-5): ");
         scanf("%d", &choice);
         
         switch(choice) {
             case 1:
-                bill += 100;
-                printf("Burger added to the bill.\n");
+            	bug();
+                bill += no;
+//                printf("Burger added to the bill.\n");
+
                 break;
             case 2:
-                bill += 180;
-                printf("Pizza added to the bill.\n");
+            	piz();
+                bill += no;
+//                printf("Pizza added to the bill.\n");
                 break;
             case 3:
-                bill += 120;
-                printf("Dosa added to the bill.\n");
+            	dos();
+                bill += no;
+//                printf("Dosa added to the bill.\n");
                 break;
             case 4:
-                bill += 50;
-                printf("Idli added to the bill.\n");
+            	idl();
+                bill += no;
+//                printf("Idli added to the bill.\n");
                 break;
             case 5:
-                printf("Thank you for using the Food Billing System.\n");
+//                printf("Thank you for using the Food Billing System.\n");
                 break;
             default:
                 printf("Invalid choice. Please try again.\n");
                 break;
         }
-    } while(choice != 5);
-    
+//  Provide the total amount of the order.  
     printf("Total bill: %.2f\n", bill);
-    
+//  Asking if they want to order again.  
     printf("\nDo you want to place order again? y or n:");
-    scanf("%c", &c);
-    if(c=='y' || c=='Y')
+    scanf("%s", &c);
+    if(c[0]=='y' || c[0]=='Y')
     {
-        goto FBS;
+        goto system;
     }
     printf("Have a nice day.");
 
